7b8d38ae2de32ab4c669fe49b19b96cf7fa3df62
Aarit Jayaswal <aaritrj@gmail.com>
2024-09-28 17:56:44 -0700

    AHHHHHHHHHHHHHHHHHHHHH

--- src/_05_for_loops/_1_my_ages/MyAges.java
+++ src/_05_for_loops/_1_my_ages/MyAges.java
@@ -10,17 +10,18 @@ public class MyAges {
 		//        Start its value at zero.
 		
 		// Change the value of yourCurrentAge to match your age
-		int yourCurrentAge = 0;
+		int age = 0;
+		int yourCurrentAge = 10;
 				
 		
-		//  WHILE LOOP. Start a while loop to repeat while age <= yourCurrentAge
-				
-			//  Print the age variable
-		
-			//  Add one to the age variable
-		
+		while (age <= yourCurrentAge) {	
+			
+			System.out.println(age);
+			
+			age+=1;
 		//  End the while loop here
-
+		}
+	
 		//  Run the program. Did it print all your ages from 0 to your current age?
 		
 		/*** Now you are going to do the same thing, but this time use a for loop. 
@@ -29,12 +30,12 @@ public class MyAges {
 		 **/
 		
 		//  FOR LOOP. Start a for loop to repeat while age <= your age, like this:
-		//  for ( age=0; age <= yourAge; age +=1 ) {
+		  for ( age=0; age <= yourCurrentAge; age +=1 ) {
 		
 			//  Print the age variable
-		
+		System.out.println(age);
 		//  End the for loop here
-		
+		  }
 		//  Run the program again. 		
 		//  Check that both loops printed the same numbers to the console
 	}
