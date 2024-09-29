24b3c3ca9fb1356d22d27edcae4c4cef0f85ba4c
Aarit Jayaswal <aaritrj@gmail.com>
2024-09-28 17:58:29 -0700

    AHHHHHHHHHHHHHHHHAHAHHAHA

---
+++ src/_05_for_loops/_2_badgers/BadgerSong.java
@@ -0,0 +1,40 @@
+package _05_for_loops._2_badgers;
+
+public class BadgerSong {
+
+	public static void main(String[] args) {
+		
+	//:3
+		
+		for(int badger=0; badger< 12; badger++) {
+
+			System.out.print("Badger");
+
+			if (badger!=11) {
+				System.out.print(", ");
+			}
+
+		}
+		System.out.println();
+
+		for(int mushroom=0; mushroom< 2; mushroom++) {
+
+			System.out.print("Mushroom");
+			if(mushroom!=1) {
+				System.out.print(", ");
+			}
+		}
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+}
