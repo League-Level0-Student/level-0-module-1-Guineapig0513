774f5eef455d55c97345ec0efd77a06b5fad4099
league <league@adminisorsimac3.attlocal.net>
2024-10-05 17:50:16 -0700

    Nacho is floof

--- src/_05_for_loops/_2_badgers/BadgerSong.java
+++ src/_05_for_loops/_2_badgers/BadgerSong.java
@@ -3,38 +3,35 @@ package _05_for_loops._2_badgers;
 public class BadgerSong {
 
 	public static void main(String[] args) {
-		
-	//:3
-		
-		for(int badger=0; badger< 12; badger++) {
 
-			System.out.print("Badger");
+		// :3
+		for (int LoopTwice = 0; LoopTwice < 2; LoopTwice++) {
 
-			if (badger!=11) {
-				System.out.print(", ");
+			for (int badger = 0; badger < 12; badger++) {
+
+				System.out.print("Badger");
+
+				if (badger != 11) {
+					System.out.print(", ");
+				}
+
+			}
+			System.out.println();
+
+			for (int mushroom = 0; mushroom < 2; mushroom++) {
+
+				System.out.print("Mushroom");
+				if (mushroom != 1) {
+					System.out.print(", ");
+				}
 			}
 
-		}
-		System.out.println();
-
-		for(int mushroom=0; mushroom< 2; mushroom++) {
-
-			System.out.print("Mushroom");
-			if(mushroom!=1) {
-				System.out.print(", ");
-			}
+			System.out.println();
+			System.out.println();
 		}
 
-
-
-
-
-
-
+		
+		System.out.print("A Snake!!!");
 	}
-
-
-
-
 
 }
---
+++ src/_05_for_loops/_4_banana_split/banana_split/banana_split.pde
@@ -0,0 +1,7 @@
+background(#D10F90);
+size(300, 300);
+textSize(35);
+for(int iceCream = 0; iceCream < 3; iceCream++){
+text("ice cream", 90 , 100+iceCream*30);
+}
+text("banana", 90 , 190);
